0897692f173655748a7ef5da6366b6c8972b03f1
laurin <georgkoltwig@gmail.com>
2025-10-14 21:40:00 +0200

    v0.6.3-BETA

--- src/main/java/de/laurinhummel/survivalfriend/events/TotemPop.java
+++ src/main/java/de/laurinhummel/survivalfriend/events/TotemPop.java
@@ -4,13 +4,14 @@ import de.laurinhummel.survivalfriend.commands.MenuSF;
 import de.laurinhummel.survivalfriend.managers.PermissionManager;
 import de.laurinhummel.survivalfriend.misc.McColors;
 import de.laurinhummel.survivalfriend.misc.SkyLogger;
-import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityResurrectEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class TotemPop implements Listener {
     @EventHandler
@@ -23,21 +24,34 @@ public class TotemPop implements Listener {
             StringBuilder sb = new StringBuilder();
                 sb.append(McColors.GOLD + player.getName() + McColors.AQUA + " was popped by ");
 
+                // IS THE DAMAGER AN ENTITY?
                 if(event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent entityEvent) {
+                    Entity damager = entityEvent.getDamager();
                     sb.append(switch (entityEvent.getCause()) {
-                        case ENTITY_EXPLOSION -> McColors.RED + "an exploding " + entityEvent.getDamager().getName();
-                        default -> McColors.RED + entityEvent.getDamager().getName();
+                        case ENTITY_EXPLOSION -> McColors.RED + "an exploding " + damager.getName();
+                        case PROJECTILE -> McColors.RED + damager.getType().name().toLowerCase() + McColors.AQUA + "'s projectile";
+                        default -> McColors.RED + damager.getName();
                     });
+
+                    // GETS ITEM NAME IF DAMAGER IS A PLAYER
+                    if(damager instanceof Player && damager != player) {
+                        ItemStack item = ((Player)damager).getInventory().getItemInMainHand();
+                        if(item.getItemMeta().getDisplayName() != item.getType().name()) {
+                            sb.append(McColors.AQUA + " using " + McColors.RED + item.getItemMeta().getDisplayName());
+                        }
+                    }
+
+                // IS THE DAMAGER NOT AN ENTITY?
                 } else {
                     sb.append(switch (entityDamageEvent.getCause()) {
                         case FALL -> McColors.RED + "hitting the ground too hard";
+                        case FIRE -> McColors.RED + "fire";
+                        case LAVA -> McColors.RED + "lava";
+                        case SUFFOCATION -> McColors.RED + "suffocation";
+                        case VOID -> McColors.RED + "the void";
                         case BLOCK_EXPLOSION -> McColors.RED + "TNT";
                         default -> McColors.RED + player.getDisplayName();
                     });
-                }
-
-                if(entityDamageEvent.getEntity() instanceof Player && entityDamageEvent.getEntity() != player) {
-                    sb.append(McColors.AQUA + " using " + McColors.RED + ((Player)entityDamageEvent.getEntity()).getInventory().getItemInMainHand().getItemMeta().getDisplayName());
                 }
 
             SkyLogger.sendServer(sb.toString(), SkyLogger.LogType.INFO);
